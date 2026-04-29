fc859f4dadc736040eccd1025512052bd51d8cb5
Jayson Domanico <jaysondomanico1@gmail.com>
2026-04-30 03:05:00 +0800

    Add spacer class; Add loading_bar and slowtpye thats within class Element

--- program_3/p3_multiline_file_writer.py
+++ program_3/p3_multiline_file_writer.py
@@ -1,4 +1,56 @@
 import os
+import time
+import sys
+
+class Spacer:
+    def __init__(self):
+        pass
+
+    def one_space(self):
+        print()
+
+    def light_space(self):
+        for i in range(2):
+            print()
+
+    def medium_space(self):
+        for i in range(3):
+            print()
+
+    def heavy_space(self):
+        for i in range(5):
+            print()
+
+    def clear_screen(self):
+        for i in range(35):
+            print()
+
+    def custom_space(self, lines=1):
+        for i in range(lines):
+            print()
+
+    def line_separator(self):
+        print("-" * 50)
+
+    def equals_separator(self):
+        print("=" * 50)
+
+class Elements:
+    def loading_bar(self, label="", total=26, duration=2):
+        for i in range(total + 1):
+            bar = '▄' * i + ' ' * (total - i)
+            percent = int((i / total) * 100)
+            sys.stdout.write(f'\r{label}{bar} {percent}%')
+            sys.stdout.flush()
+            time.sleep(duration / total)
+        print()
+    
+    def slowtype(self, text, duration):
+        delay = duration / len(text) if len(text) > 0 else 0
+        for char in text:
+            print(char, end='', flush=True)
+            time.sleep(delay)
+        print()
 
 class FileWriter:
     def __init__(self, output_file):
